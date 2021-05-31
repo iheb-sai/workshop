3862dd2a5ccdc2c6d645162f5202bc346c837d5c
Iheb Rahali <iheb.rahali@sense-and-image.com>
2021-05-31 17:40:34 +0200

    init

---
+++ AppLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Iheb Rahali on 31.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        view.backgroundColor = .red
+    }
+
+
+}
+
